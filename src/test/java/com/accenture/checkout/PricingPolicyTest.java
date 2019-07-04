da3891db874f978f9dc8bd2a46b170116315a54c
Dylan Moreland <stablemap@icloud.com>
2019-07-04 10:28:30 -0400

    Start porting unit pricing tests from Order up to PricingPolicy

--- src/test/java/com/accenture/checkout/PricingPolicyTest.java
+++ src/test/java/com/accenture/checkout/PricingPolicyTest.java
@@ -1,5 +1,6 @@
 package com.accenture.checkout;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,26 @@ class PricingPolicyTest {
     assertThatThrownBy(() -> pricingPolicy.setPricePerUnit("watermelon", -227))
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessageContaining("-227");
+  }
+
+  @Test
+  void priceForUnits_noUnits_isZero() {
+    pricingPolicy.setPricePerUnit("watermelon", 227);
+
+    assertThat(pricingPolicy.priceForUnits("watermelon", 0)).isZero();
+  }
+
+  @Test
+  void priceForUnits_oneUnit_isUnitPrice() {
+    pricingPolicy.setPricePerUnit("watermelon", 227);
+
+    assertThat(pricingPolicy.priceForUnits("watermelon", 1)).isEqualTo(227);
+  }
+
+  @Test
+  void priceForUnits_twoUnits_isTwiceUnitPrice() {
+    pricingPolicy.setPricePerUnit("watermelon", 227);
+
+    assertThat(pricingPolicy.priceForUnits("watermelon", 2)).isEqualTo(454);
   }
 }
